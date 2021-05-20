b8ceb9c70fa015008e8ab3553ae354091ebb78a8
nasenag <nasenag16@gmail.com>
2021-05-21 07:14:58 +0900

    Update

---
+++ update.sh
@@ -0,0 +1,8 @@
+ #bin/bash
+ mdhtml index.md -t template.html -o index.html
+ 
+ mdhtml privacy/index.md -t template.html -o privacy/index.html
+ 
+ mdhtml terms/index.md -t template.html -o terms/index.html
+ 
+ 
